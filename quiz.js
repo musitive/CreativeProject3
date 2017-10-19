e1ad52caaf00e0027f1dfbf63ef9c5ae17d88e3c
musitive <dallinfrank@gmail.com>
2017-10-19 17:40:58 -0600

    lol

--- quiz.js
+++ quiz.js
@@ -17,7 +17,200 @@ function($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('questions');
 }])
+.controller('testCtrl', function($scope) {
+    $scope.currentSelection = 0;
+    $scope.questionNumber = 0;
+    $scope.totalPoints = 0;
+    $scope.questions = [
+        {
+            title: "What are you most likely to do on a Saturday night?",
+            responses: [
+                {
+                    text: "Don't Know",
+                    points: 0
+                },
+                {
+                    text: "Me too thanks",
+                    points: 8
+                },
+                {
+                    text: "Go to Institute",
+                    points: 6
+                },
+                {
+                    text: "Hit up the club",
+                    points: 4
+                },
+                {
+                    text: "Watch TV",
+                    points: 2
+                }
+            ]
+        },
+        {
+            title: "What is your favorite color?",
+            responses: [
+                {
+                    text: "Don't know",
+                    points: 8
+                },
+                {
+                    text: "Magenta",
+                    points: 2
+                },
+                {
+                    text: "Orange",
+                    points: 4
+                },
+                {
+                    text: "Neon Pink",
+                    points: 6
+                },
+                {
+                    text: "blue",
+                    points: 0
+                }
+            ]
+        },
+        {
+            title: "What type of phone do you have?",
+            responses: [
+                {
+                    text: "Windows",
+                    points: 0
+                },
+                {
+                    text: "Landline",
+                    points: 6
+                },
+                {
+                    text: "Bongo Phone",
+                    points: 8
+                },
+                {
+                    text: "Nokia",
+                    points: 2
+                },
+                {
+                    text: "Android/iPhone",
+                    points: 2
+                }
+            ]
+        },
+        {
+            title: "What are you most likely to do on a Saturday night?",
+            responses: [
+                {
+                    text: "Don't Know",
+                    points: 0
+                },
+                {
+                    text: "Me too thanks",
+                    points: 2
+                },
+                {
+                    text: "Go to Institute",
+                    points: 4
+                },
+                {
+                    text: "Hit up the club",
+                    points: 6
+                },
+                {
+                    text: "Watch TV",
+                    points: 8
+                }
+            ]
+        },
+        {
+            title: "What are you most likely to do on a Saturday night?",
+            responses: [
+                {
+                    text: "Don't Know",
+                    points: 0
+                },
+                {
+                    text: "Me too thanks",
+                    points: 2
+                },
+                {
+                    text: "Go to Institute",
+                    points: 4
+                },
+                {
+                    text: "Hit up the club",
+                    points: 6
+                },
+                {
+                    text: "Watch TV",
+                    points: 8
+                }
+            ]
+        },
+        {
+            title: "What are you most likely to do on a Saturday night?",
+            responses: [
+                {
+                    text: "Don't Know",
+                    points: 0
+                },
+                {
+                    text: "Me too thanks",
+                    points: 2
+                },
+                {
+                    text: "Go to Institute",
+                    points: 4
+                },
+                {
+                    text: "Hit up the club",
+                    points: 6
+                },
+                {
+                    text: "Watch TV",
+                    points: 8
+                }
+            ]
+        },
+        {
+            title: "What are you most likely to do on a Saturday night?",
+            responses: [
+                {
+                    text: "Don't Know",
+                    points: 0
+                },
+                {
+                    text: "Me too thanks",
+                    points: 2
+                },
+                {
+                    text: "Go to Institute",
+                    points: 4
+                },
+                {
+                    text: "Hit up the club",
+                    points: 6
+                },
+                {
+                    text: "Watch TV",
+                    points: 8
+                }
+            ]
+        }
+    ];
+    //$scope.currentQuestion = questions[0].title;
 
+    $scope.nextQuestion = function() {
+        $scope.questionNumber++;
+    }
+
+    $scope.addPoints = function() {
+        $scope.totalPoints += $scope.questions[$scope.questionNumber].responses[$scope.currentSelection].points;
+        console.log("Current Selection Points: " + $scope.questions[$scope.questionNumber].responses[$scope.currentSelection].points);
+        console.log("Total Points: " + $scope.totalPoints);
+        $scope.nextQuestion();       
+    };
+})
 
 
 /*
